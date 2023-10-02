32995acb2bb38086efe49a41196432bba0ccd0ef
yannic rozon <yannicrozon@yahoo.ca>
2023-10-02 14:41:16 -0400

    Ajout dans fichier js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -16,4 +16,63 @@ nav();
 
 
 
+const slides = document.querySelectorAll ('.slide');
+
+const prevBtn = document.querySelector('.icon-chevron-thin-left');
+const nextBtn = document.querySelector('.icon-chevron-thin-right');
+
+
+/*
+slides.forEach (function (slide, index) {
+	slides.style.left = `${index * 100}%`;
+  }); */
+
+
+let counter = 0;
+
+prevBtn.addEventListener ('click', function () {
+	counter--;
+	carousel ();
+  });
+
+  nextBtn.addEventListener ('click', function () {
+	counter++;
+	carousel ();
+  });
+
+  function carousel(){
+	if(counter === slides.length ){
+	
+	counter = 0
+	
+	}
+	
+	if(counter < 0) {
+	counter = slides.length -1;
+	
+	} 
+
+	slides.forEach (function (slide) {
+
+		slide.style.opacity = `1`;
+		
+	  }); 
+	}
+
+
+/*
 const slides = document.querySelector();
+
+modalBtn.addEventListener('click', function(){
+
+    modal.classList.add('open-modal')
+
+})
+
+closeBtn.addEventListener('click', function() {
+
+    modal.classList.remove('open-modal')
+
+})
+
+*/
